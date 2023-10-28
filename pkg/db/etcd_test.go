9f20fe8203ee789597294e946d3f5c4f53f221b0
Maho Takara <takara9@gmail.com>
2023-10-28 09:08:58 +0000

    update db test

--- pkg/db/etcd_test.go
+++ pkg/db/etcd_test.go
@@ -3,25 +3,24 @@ package db
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-
-	"db"
-	"errors"
+	etcd "go.etcd.io/etcd/client/v3"
+	//"errors"
 )
 
 var _ = Describe("Etcd", func() {
 
 	const url = "http://127.0.0.1:2379"
 	const url_noexist = "http://127.0.0.1:2380"
-	var testv = 12
+	//var testv string = 12
 	var Conn *etcd.Client
-	var err errors
+	var err error
 
 	Describe("Test etcd", func() {
 		Context("Test Connection to etcd", func() {
-			It("Connection etcd not exist", func() {
-				Conn, err = Connect(url_noexist)
-				Expect(err).To(HaveOccurred())
-		  	})
+			//It("Connection etcd not exist", func() {
+			//	conn, err: = Connect(url_noexist)
+			//	Expect(err).To(HaveOccurred())
+		  	//})
 			It("Connection etcd", func() {
 				Conn, err = Connect(url)
 				Expect(err).NotTo(HaveOccurred())
@@ -29,12 +28,12 @@ var _ = Describe("Etcd", func() {
 		})
 
 		Context("Test access etcd", func() {
-			var key_data = "hv01" 
-			var data_hv1 Hypervisor
+			var key_hv1 = "hv01" 
+			//var data_get Hypervisor
+			data_hv1 := testHvData1()
 
 			It("Put", func() {
-				data_hv1 := testHvData1()
-				err := PutDataEtcd(Conn, key_data, data_hv1)
+				err := PutDataEtcd(Conn, key_hv1, data_hv1)
 				Expect(err).NotTo(HaveOccurred())
 		  	})
 
@@ -45,12 +44,12 @@ var _ = Describe("Etcd", func() {
 		  	})
 
   	        It("Del", func() {
-				err = DelByKey(Conn, key)	
+				err = DelByKey(Conn, key_hv1)	
 				Expect(err).NotTo(HaveOccurred())
 		  	})
 
 		  	It("Check delete data", func() {
-				data_get, err := GetHvByKey(Conn, key_hv1)
+				_, err := GetHvByKey(Conn, key_hv1)
 				Expect(err).To(HaveOccurred())
 		  	})
 		})
--- pkg/db/go.mod
+++ pkg/db/go.mod
@@ -4,6 +4,8 @@ go 1.19
 
 require (
 	github.com/google/uuid v1.3.0
+	github.com/onsi/ginkgo/v2 v2.13.0
+	github.com/onsi/gomega v1.29.0
 	go.etcd.io/etcd/client/v3 v3.5.7
 
 )
@@ -11,17 +13,23 @@ require (
 require (
 	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
+	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38 // indirect
 	go.etcd.io/etcd/api/v3 v3.5.7 // indirect
 	go.etcd.io/etcd/client/pkg/v3 v3.5.7 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.17.0 // indirect
-	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
-	golang.org/x/sys v0.0.0-20210603081109-ebe580a85c40 // indirect
-	golang.org/x/text v0.3.5 // indirect
+	golang.org/x/net v0.17.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/tools v0.12.0 // indirect
 	google.golang.org/genproto v0.0.0-20210602131652-f16073e35f0c // indirect
 	google.golang.org/grpc v1.41.0 // indirect
-	google.golang.org/protobuf v1.26.0 // indirect
+	google.golang.org/protobuf v1.28.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
