1018cc6201adad056145a313b671976b91fa1104
Maho Takara <tkr9955@gmail.com>
2025-07-24 23:01:48 +0000

    バグ修正

--- cmd/vm-client/vm-client.go
+++ cmd/vm-client/vm-client.go
@@ -69,13 +69,6 @@ func main() {
 			GlobalListVm(ApiUrl)
 			fmt.Printf("\n")
 			return
-		default:
-			fmt.Printf("\n               *** SYSTEM STATUS ***\n")
-			ListHv(ApiUrl)
-			fmt.Printf("\n")
-			GlobalListVm(ApiUrl)
-			fmt.Printf("\n")
-			return
 		}
 	}
 
