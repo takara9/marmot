623eeeb0604f14f7630bb2900117b0a2292d3a08
Maho Takara <tkr9955@gmail.com>
2025-08-30 06:54:27 +0000

    関数名の修正

--- pkg/marmot/cluster-stop.go
+++ pkg/marmot/cluster-stop.go
@@ -1,18 +1,11 @@
 package marmot
 
 import (
-	"bytes"
-	"encoding/json"
 	"errors"
-	"fmt"
-	"io"
 	"log/slog"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 	cf "github.com/takara9/marmot/pkg/config"
-	"github.com/takara9/marmot/pkg/db"
-	"github.com/takara9/marmot/pkg/virt"
 )
 
 // クラスタの停止
@@ -44,7 +37,7 @@ func (m *marmot) stopCluster(cnf cf.MarmotConfig) error {
 				slog.Error("", "err", err)
 				continue
 			}
-			err = remoteStopVM(vm.HvNode, spec)
+			err = stopRemoteVM(vm.HvNode, spec)
 			if err != nil {
 				slog.Error("", "err", err)
 				continue
@@ -53,84 +46,6 @@ func (m *marmot) stopCluster(cnf cf.MarmotConfig) error {
 	}
 	if NotFound {
 		return errors.New("NotExistVM")
-	}
-	return nil
-}
-
-func remoteStopVM(hvNode string, spec cf.VMSpec) error {
-	byteJSON, _ := json.MarshalIndent(spec, "", "    ")
-	// JSON形式でポストする
-	reqURL := fmt.Sprintf("http://%s:8750/%s", hvNode, "stopVm")
-	request, err := http.NewRequest("POST", reqURL, bytes.NewBuffer(byteJSON))
-	if err != nil {
-		slog.Error("", "err", err)
-		return err
-	}
-
-	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
-
-	client := &http.Client{}
-	response, err := client.Do(request)
-	if err != nil {
-		slog.Error("", "err", err)
-		return err
-	}
-	defer response.Body.Close()
-	body, _ := io.ReadAll(response.Body)
-	if response.StatusCode != 200 {
-		return errors.New(string(body))
-	}
-	return nil
-}
-
-// 仮想マシンの停止
-func (m *marmot) StopVm(c *gin.Context) {
-	slog.Info("stop vm", "etcd", m.EtcdUrl)
-	var spec cf.VMSpec
-	err := c.BindJSON(&spec)
-	if err != nil {
-		slog.Error("setup config", "err", err)
-		return
-	}
-	err = m.stopVM(spec)
-	if err != nil {
-		slog.Error("stop vm", "err", err)
-		c.JSON(400, gin.H{"msg": err.Error()})
-		return
-	}
-}
-
-// VMの停止
-func (m *marmot) stopVM(spec cf.VMSpec) error {
-	vm, err := m.Db.GetVmByKey(spec.Key)
-	if err != nil {
-		slog.Error("", "err", err)
-		return nil
-	}
-
-	if vm.Status == db.RUNNING {
-		// 仮想マシンの停止＆削除
-		url := "qemu:///system"
-		err = virt.StopVM(url, spec.Key)
-		if err != nil {
-			slog.Error("", "err", err)
-		}
-		// ハイパーバイザーのリソース削減保存
-		hv, err := m.Db.GetHvByKey(vm.HvNode)
-		if err != nil {
-			slog.Error("", "err", err)
-		}
-		hv.FreeCpu = hv.FreeCpu + vm.Cpu
-		hv.FreeMemory = hv.FreeMemory + vm.Memory
-		err = m.Db.PutDataEtcd(hv.Key, hv)
-		if err != nil {
-			slog.Error("", "err", err)
-		}
-		// データベースの更新
-		err = m.Db.UpdateVmState(spec.Key, db.STOPPED) ////////
-		if err != nil {
-			slog.Error("", "err", err)
-		}
 	}
 	return nil
 }
---
+++ pkg/marmot/vm-start.go
@@ -0,0 +1,103 @@
+package marmot
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	cf "github.com/takara9/marmot/pkg/config"
+	"github.com/takara9/marmot/pkg/db"
+	"github.com/takara9/marmot/pkg/virt"
+)
+
+// 仮想マシンの開始
+func (m *marmot) StartVm(c *gin.Context) {
+	slog.Info("start vm", "etcd", m.EtcdUrl)
+	var spec cf.VMSpec
+	err := c.BindJSON(&spec)
+	if err != nil {
+		slog.Error("setup config", "err", err)
+		return
+	}
+	err = startVM(m.EtcdUrl, spec)
+	if err != nil {
+		slog.Error("start vm", "err", err)
+		c.JSON(400, gin.H{"msg": err.Error()})
+		return
+	}
+}
+
+// VMの開始
+func startVM(dbUrl string, spec cf.VMSpec) error {
+	// 仮想マシンの開始
+	url := "qemu:///system"
+	err := virt.StartVM(url, spec.Key)
+	if err != nil {
+		slog.Error("", "err", err)
+	}
+
+	d, err := db.NewDatabase(dbUrl)
+	if err != nil {
+		slog.Error("", "err", err)
+		return err
+	}
+
+	vm, err := d.GetVmByKey(spec.Key)
+	if err != nil {
+		slog.Error("", "err", err)
+	}
+
+	if vm.Status == db.STOPPED {
+		// ハイパーバイザーのリソースの減算と保存
+		hv, err := d.GetHvByKey(vm.HvNode)
+		if err != nil {
+			slog.Error("", "err", err)
+		}
+		hv.FreeCpu = hv.FreeCpu - vm.Cpu
+		hv.FreeMemory = hv.FreeMemory - vm.Memory
+
+		err = d.PutDataEtcd(hv.Key, hv)
+		if err != nil {
+			slog.Error("", "err", err)
+		}
+		// データベースの更新
+		err = d.UpdateVmState(spec.Key, db.RUNNING)
+		if err != nil {
+			slog.Error("", "err", err)
+		}
+	}
+	return nil
+}
+
+func startRemoteVM(hvNode string, spec cf.VMSpec) error {
+	byteJSON, _ := json.MarshalIndent(spec, "", "    ")
+	// JSON形式でポストする
+	reqURL := fmt.Sprintf("http://%s:8750/%s", hvNode, "startVm")
+	request, err := http.NewRequest("POST", reqURL, bytes.NewBuffer(byteJSON))
+	if err != nil {
+		slog.Error("", "err", err)
+		return err
+	}
+
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+
+	client := &http.Client{}
+	response, err := client.Do(request)
+	if err != nil {
+		slog.Error("", "err", err)
+		return err
+	}
+	defer response.Body.Close()
+
+	// レスポンスを取得する
+	body, _ := io.ReadAll(response.Body)
+	if response.StatusCode != 200 {
+		return errors.New(string(body))
+	}
+	return nil
+}
