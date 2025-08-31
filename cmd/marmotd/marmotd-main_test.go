532c9cdb366fe11264847b782f0d888e945c5451
Maho Takara <tkr9955@gmail.com>
2025-08-31 01:39:00 +0000

    テストヘルパーの追加、marmotのエクスポート
    Signed-off-by: Maho Takara <tkr9955@gmail.com>

--- cmd/marmotd/marmotd-main_test.go
+++ cmd/marmotd/marmotd-main_test.go
@@ -1,64 +1,58 @@
 package main
 
 import (
+	"fmt"
+	"os/exec"
 	"time"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
 	"github.com/takara9/marmot/pkg/config"
+	"github.com/takara9/marmot/pkg/marmot"
 	//clientv3 "go.etcd.io/etcd/client/v3"
 )
 
 var _ = Describe("Marmotd Test", Ordered, func() {
 	var err error
-	//var containerID string
+	var containerID string
 	//var Conn *clientv3.Client
+	var ep *marmot.Marmot
 
 	BeforeAll(func(ctx SpecContext) {
 		GinkgoWriter.Println("Start marmot server mock")
-		//startMockServer() // 戻り値なし
+		startMockServer() // 戻り値なし
 		time.Sleep(5 * time.Second)
+		ep, err = marmot.NewMarmot("hvc", "http://127.0.0.1:12379")
+		if err != nil {
+			GinkgoWriter.Println("Error creating MarmotEndpoint:", err)
+		} else {
+			GinkgoWriter.Println("MarmotEndpoint created successfully:", ep)
+		}
 
-		// 起動チェック
-		/*
-			ep, err = NewMarmotdEp(
-				"http",
-				"127.0.0.1:8080",
-				"/api/v1",
-				10)
-			if err != nil {
-				GinkgoWriter.Println("Error creating MarmotEndpoint:", err)
-			} else {
-				GinkgoWriter.Println("MarmotEndpoint created successfully:", ep)
-			}
-
-			// Dockerコンテナを起動
-			cmd := exec.Command("docker", "run", "-d", "--name", "etcd0", "-p", "12379:2379", "-p", "12380:2380", "-e", "ALLOW_NONE_AUTHENTICATION=yes", "-e", "ETCD_ADVERTISE_CLIENT_URLS=http://127.0.0.1:12379", "bitnami/etcd")
-			output, err := cmd.CombinedOutput()
-			if err != nil {
-				Fail(fmt.Sprintf("Failed to start container: %s, %v", string(output), err))
-			}
-			containerID = string(output[:12]) // 最初の12文字をIDとして取得
-			fmt.Printf("Container started with ID: %s\n", containerID)
-		*/
+		// Dockerコンテナを起動
+		cmd := exec.Command("docker", "run", "-d", "--name", "etcd0", "-p", "12379:2379", "-p", "12380:2380", "-e", "ALLOW_NONE_AUTHENTICATION=yes", "-e", "ETCD_ADVERTISE_CLIENT_URLS=http://127.0.0.1:12379", "bitnami/etcd")
+		output, err := cmd.CombinedOutput()
+		if err != nil {
+			Fail(fmt.Sprintf("Failed to start container: %s, %v", string(output), err))
+		}
+		containerID = string(output[:12]) // 最初の12文字をIDとして取得
+		fmt.Printf("Container started with ID: %s\n", containerID)
 		time.Sleep(10 * time.Second) // コンテナが起動するまで待機
 	}, NodeTimeout(20*time.Second))
 
 	AfterAll(func(ctx SpecContext) {
-		/*
-			// Dockerコンテナを停止・削除
-			cmd := exec.Command("docker", "stop", containerID)
-			_, err := cmd.CombinedOutput()
-			if err != nil {
-				fmt.Printf("Failed to stop container: %v\n", err)
-			}
-			cmd = exec.Command("docker", "rm", containerID)
-			_, err = cmd.CombinedOutput()
-			if err != nil {
-				fmt.Printf("Failed to remove container: %v\n", err)
-			}
-		*/
+		// Dockerコンテナを停止・削除
+		cmd := exec.Command("docker", "stop", containerID)
+		_, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Printf("Failed to stop container: %v\n", err)
+		}
+		cmd = exec.Command("docker", "rm", containerID)
+		_, err = cmd.CombinedOutput()
+		if err != nil {
+			fmt.Printf("Failed to remove container: %v\n", err)
+		}
 	}, NodeTimeout(20*time.Second))
 
 	Context("基本的なアクセステスト", func() {
