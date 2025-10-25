5999b21d31aff62c19d69a42f665f96e52a5b71e
Maho Takara <tkr9955@gmail.com>
2025-10-25 01:08:18 +0000

    型の変更ミスの修正

--- pkg/config/config_test.go
+++ pkg/config/config_test.go
@@ -20,7 +20,7 @@ var _ = Describe("Config", func() {
 			It("Read existing file", func() {
 				err := ReadConfig(input1, &mc)
 				Expect(err).NotTo(HaveOccurred())
-				Expect(mc.Domain).To(Equal("labo.local"))
+				Expect(*mc.Domain).To(Equal("labo.local"))
 				//Expect(mc.VMSpec[0].Name).To(Equal("node1"))
 			})
 			It("Read no existing file", func() {
