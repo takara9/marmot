e32974a9a6d6b8e486ff8173a16a57bf1e1fb970
Maho Takara <takara9@gmail.com>
2025-10-30 07:34:28 +0900

    maadmコマンドの実装

---
+++ cmd/maadm/cmd/version.go
@@ -0,0 +1,37 @@
+package cmd
+
+import (
+	"fmt"
+	"log/slog"
+
+	"github.com/spf13/cobra"
+	"github.com/takara9/marmot/pkg/config"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "バージョンの表示",
+	Long:  `maadm クライアントのバージョンを表示します。`,
+	Run: func(cmd *cobra.Command, args []string) {
+		slog.Debug("===", "versionCmd is called", "===")
+
+		m, err := config.GetClientConfig2(apiConfigFilename)
+		if err != nil {
+			slog.Error("version", "err", err)
+			return
+		}
+
+		JsonVersion, err := m.GetVersion()
+		if err != nil {
+			slog.Error("version", "err", err)
+			return
+		}
+
+		fmt.Println(string(JsonVersion.Version))
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
