50bec62c723724b2e3ecb7ed6d5d6d88337410c3
Maho Takara <takara9@gmail.com>
2025-11-16 18:50:47 +0900

    デバックオプションの修正

--- cmd/install.sh
+++ cmd/install.sh
@@ -5,20 +5,24 @@ DISTDIR=/usr/local/marmot
 SERVER_EXE=marmotd
 CLIENT_CMD=mactl
 CLIENT_CONFIG=config_marmot
-ADMIN_CMD=hv-admin
+ADMIN_CMD=maadm
 
 rm -fr ${DISTDIR}
 mkdir ${DISTDIR}
 install -m 0755 ${BINDIR}/${SERVER_EXE} ${DISTDIR}
 install -m 0644 ${BINDIR}/temp.xml ${DISTDIR}
+
 rm -f /etc/systemd/system/marmot.service
+HOSTNAME=`hostname`
+sed -i s/XXXHOSTXXXX/${HOSTNAME} ${BINDIR}/marmot.service
 install -m 0644 ${BINDIR}/marmot.service /etc/systemd/system
+
 rm -f /usr/local/bin/${CLIENT_CMD}
 install -m 0755 ${BINDIR}/${CLIENT_CMD} /usr/local/bin
 rm -f /usr/local/bin/${ADMIN_CMD}
 install -m 0755 ${BINDIR}/${ADMIN_CMD} /usr/local/bin
 install -m 0644 ${BINDIR}/${CLIENT_CONFIG} ${HOME}/.${CLIENT_CONFIG}
 
-
+systemctl daemon-reload
 systemctl enable marmot
 systemctl start marmot
--- cmd/mactl/test-helper.go
+++ cmd/mactl/test-helper.go
@@ -17,7 +17,7 @@ func startMockServer() *marmotd.Server {
 		// Setup slog
 		opts := &slog.HandlerOptions{
 			AddSource: true,
-			Level:     slog.LevelDebug,
+			//Level:     slog.LevelDebug,
 		}
 		logger := slog.New(slog.NewJSONHandler(os.Stderr, opts))
 		slog.SetDefault(logger)
