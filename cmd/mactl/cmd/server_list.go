3c65a2c0ff3fc5276fbd5fcfee87f44855984c64
Maho Takara <takara9@gmail.com>
2026-01-28 19:22:18 +0900

    パニックの対処

--- cmd/mactl/cmd/server_list.go
+++ cmd/mactl/cmd/server_list.go
@@ -32,7 +32,7 @@ var serverListCmd = &cobra.Command{
 				fmt.Printf("Server %d:", idx+1)
 				fmt.Printf("  ID: %v", serverMap["id"])
 				fmt.Printf("  Name: %v", serverMap["name"])
-				fmt.Printf("  Status: %v", serverMap["status"].(int))
+				fmt.Printf("  Status: %v", serverMap["status"])
 				fmt.Printf("  CPU: %v", serverMap["cpu"])
 				fmt.Printf("  Memory: %v MB", serverMap["memory"])
 				fmt.Println()
