40a2d83408c86e7537d4531466d23a46c2907f24
Maho Takara <takara9@gmail.com>
2026-02-17 19:32:10 +0900

    仮想ネットワーク機能の開発

--- pkg/marmotd/marmotd-network.go
+++ pkg/marmotd/marmotd-network.go
@@ -8,21 +8,30 @@ import (
 
 // Get Network Information
 // (GET /network)
-func (s *Server) GetNetworks(ctx echo.Context) error
+func (s *Server) GetNetworks(ctx echo.Context) error {
+	return nil
+}
 
 // Create Virtual Network
 // (POST /network)
-func (s *Server) CreateNetwork(ctx echo.Context) error
+func (s *Server) CreateNetwork(ctx echo.Context) error {
+	return nil
+}
 
 // Delete Virtual Network
 // (DELETE /network/{id})
-func (s *Server) DeleteNetworkById(ctx echo.Context, id string) error
+func (s *Server) DeleteNetworkById(ctx echo.Context, id string) error {
+	return nil
+}
 
 // Get particular virtual network Information
 // (GET /network/{id})
-func (s *Server) GetNetworkById(ctx echo.Context, id string) error
+func (s *Server) GetNetworkById(ctx echo.Context, id string) error {
+	return nil
+}
 
 // Update Virtual Network Information by Id
 // (PUT /network/{id})
-func (s *Server) UpdateNetworkById(ctx echo.Context, id string) error
-
+func (s *Server) UpdateNetworkById(ctx echo.Context, id string) error {
+	return nil
+}
