2cfdcf1a936a0a802dcd3b1b257bd1b8a616f82a
Maho Takara <takara9@gmail.com>
2026-02-22 19:20:54 +0900

    テストスペックの実装-1

--- pkg/marmotd/marmotd-network.go
+++ pkg/marmotd/marmotd-network.go
@@ -9,6 +9,7 @@ import (
 )
 
 // クライアントから仮想ネットワークの情報を受け取って、仮想ネットワークを作成する
+// ここでは、etcdに書き込むだけで、実際に仮想ネットワークを作成するのはコントローラー側で行う
 func (s *Server) CreateNetwork(ctx echo.Context) error {
 	// クライアントから仮想ネットワークの情報を受け取る
 	var spec api.VirtualNetwork
@@ -28,27 +29,8 @@ func (s *Server) CreateNetwork(ctx echo.Context) error {
 	return ctx.JSON(201, network)
 }
 
-// 仮想ネットワークのリストを取得して返す
-func (s *Server) GetNetworks(ctx echo.Context) error {
-	networks, err := s.Ma.Db.GetVirtualNetworks()
-	if err != nil {
-		slog.Error("failed to get virtual networks", "err", err)
-		return echo.NewHTTPError(500, "failed to get virtual networks")
-	}
-	return ctx.JSON(200, networks)
-}
-
-// 仮想ネットワークをIDで取得する
-func (s *Server) GetNetworkById(ctx echo.Context, id string) error {
-	network, err := s.Ma.Db.GetVirtualNetworkById(id)
-	if err != nil {
-		slog.Error("failed to get virtual network", "err", err, "networkId", id)
-		return echo.NewHTTPError(500, "failed to get virtual network")
-	}
-	return ctx.JSON(200, network)
-}
-
 // 仮想ネットワークをIDで削除する
+// 実際に削除するのはコントローラー側で、ここではDBに削除タイムスタンプを設定するだけ
 func (s *Server) DeleteNetworkById(ctx echo.Context, id string) error {
 	if err := s.Ma.Db.DeleteVirtualNetworkById(id); err != nil {
 		slog.Error("failed to delete virtual network", "err", err, "networkId", id)
@@ -57,7 +39,9 @@ func (s *Server) DeleteNetworkById(ctx echo.Context, id string) error {
 	return ctx.NoContent(204)
 }
 
+
 // クライアントから仮想ネットワークの情報を受け取って、仮想ネットワークをIDで更新する
+// ここでは、etcdに書き込むだけで、実際に仮想ネットワークを更新するのはコントローラー側で行う
 func (s *Server) UpdateNetworkById(ctx echo.Context, id string) error {
 	var spec api.VirtualNetwork
 	if err := ctx.Bind(&spec); err != nil {
@@ -72,3 +56,27 @@ func (s *Server) UpdateNetworkById(ctx echo.Context, id string) error {
 
 	return ctx.NoContent(204)
 }
+
+
+// 参照のみ
+// 仮想ネットワークのリストを取得して返す
+func (s *Server) GetNetworks(ctx echo.Context) error {
+	networks, err := s.Ma.Db.GetVirtualNetworks()
+	if err != nil {
+		slog.Error("failed to get virtual networks", "err", err)
+		return echo.NewHTTPError(500, "failed to get virtual networks")
+	}
+	return ctx.JSON(200, networks)
+}
+
+// 参照のみ
+// 仮想ネットワークをIDで取得する
+func (s *Server) GetNetworkById(ctx echo.Context, id string) error {
+	network, err := s.Ma.Db.GetVirtualNetworkById(id)
+	if err != nil {
+		slog.Error("failed to get virtual network", "err", err, "networkId", id)
+		return echo.NewHTTPError(500, "failed to get virtual network")
+	}
+	return ctx.JSON(200, network)
+}
+
