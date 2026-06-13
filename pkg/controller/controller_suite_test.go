ec9114805d32df63dd18f7e6af67c89cbf8fc105
Maho Takara <takara9@gmail.com>
2026-06-13 13:15:09 +0900

    コントローラーのテストスタイルの変更

---
+++ pkg/controller/controller_suite_test.go
@@ -0,0 +1,13 @@
+package controller
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestController(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Controller Suite")
+}
