898968da39c1c04ba972e4e632f5930799160a0f
Maho Takara <takara9@gmail.com>
2026-07-10 11:26:08 +0900

    ST1005 / S1009 の指定分を修正

--- cmd/mactl/cmd/image_create.go
+++ cmd/mactl/cmd/image_create.go
@@ -39,10 +39,10 @@ var imageCreateCmd = &cobra.Command{
 		}
 
 		if strings.TrimSpace(image.Metadata.Name) == "" {
-			return fmt.Errorf("Metadata.name is required in the configuration")
+			return fmt.Errorf("metadata.name is required in the configuration")
 		}
 		if image.Spec.SourceUrl == nil || strings.TrimSpace(*image.Spec.SourceUrl) == "" {
-			return fmt.Errorf("Spec.source_url is required in the configuration")
+			return fmt.Errorf("spec.source_url is required in the configuration")
 		}
 
 		byteBody, _, err := m.CreateImage(image)
